e90db19c5a31f0b9915d9f7f2254c656617267cf
staszad <stas_ady@protonmail.com>
2022-01-27 13:13:28 +0100

    added exaples of interpolation, binding and conditionals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = '2022.01.27_basics';
+  title = 'Title - from property';
+  title1 = 'Empty field';
+  dog = {
+    nickname: 'reksio',
+  };
+  getInfo() {
+    return 'Yout info - from method';
+  }
+
+  get message() {
+    return "Message for you - from 'get'";
+  }
+
+  get number() {
+    return Math.floor(Math.random() * 15);
+  }
+
+  numToCheck: number = 0;
+  isDividable(num: number, divisor: number) {
+    return !(num % divisor);
+  }
 }
